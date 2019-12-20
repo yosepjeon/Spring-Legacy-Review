655cec43aeccdce8e62518f1d31ba8c8ff282004
yosepjeon <enekelx1@naver.com>
2019-12-20 20:00:09 +0900

    websocket STOMP 연결 비연결 확인 복습

--- src/main/java/com/yosep/spring/config/WebSocketMessageBrokerConfig.java
+++ src/main/java/com/yosep/spring/config/WebSocketMessageBrokerConfig.java
@@ -1,22 +1,34 @@
 package com.yosep.spring.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import com.yosep.spring.socket.handler.StompHandler;
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketMessageBrokerConfig implements WebSocketMessageBrokerConfigurer{
+	@Autowired
+    private StompHandler stompHandler;
+	
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stompTest").setAllowedOrigins("*").withSockJS();
-    }
+		registry.addEndpoint("/stompTest").setAllowedOrigins("*").withSockJS();
+	}
 	
 	@Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
 	   registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/");
+    }
+	
+	@Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
     }
 }
---
+++ src/main/java/com/yosep/spring/socket/handler/StompHandler.java
@@ -0,0 +1,33 @@
+package com.yosep.spring.socket.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptorAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StompHandler extends ChannelInterceptorAdapter {
+    private final Logger logger = LoggerFactory.getLogger(StompHandler.class);
+
+    @Override
+    public void postSend(Message message, MessageChannel channel, boolean sent) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        String sessionId = accessor.getSessionId();
+        switch (accessor.getCommand()) {
+            case CONNECT:
+                // 유저가 Websocket으로 connect()를 한 뒤 호출됨
+            	System.out.println("connected");
+                break;
+            case DISCONNECT:
+                // 유저가 Websocket으로 disconnect() 를 한 뒤 호출됨 or 세션이 끊어졌을 때 발생함(페이지 이동~ 브라우저 닫기 등)
+                System.out.println("disconnected");
+            	break;
+            default:
+                break;
+        }
+
+    }
+}
